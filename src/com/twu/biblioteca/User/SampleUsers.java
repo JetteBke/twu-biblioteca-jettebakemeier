1c3d08ce691b5ec4a1179d5ea0ee30258d89f3f6
Jette Bakemeier <jette.bakemeier@thoughtworks.com>
2019-10-18 10:24:55 +0200

    [Jette] [2.3] fixed bug in User Sample

--- src/com/twu/biblioteca/User/SampleUsers.java
+++ src/com/twu/biblioteca/User/SampleUsers.java
@@ -1,7 +1,5 @@
 package com.twu.biblioteca.User;
 
-import com.twu.biblioteca.Books.Book;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +8,13 @@ public class SampleUsers {
     public List<User> Sample() {
         User userOne = new User("234-2375", "no");
         User userTwo = new User("123-1234", "12345678");
+        User userThree = new User("111-1111", "123");
+        User userFour = new User("222-2222", "111");
         List<User> myUsers = new ArrayList<User>();
         myUsers.add(userOne);
         myUsers.add(userTwo);
+        myUsers.add(userThree);
+        myUsers.add(userFour);
         return myUsers;
     }
 }
--- src/com/twu/biblioteca/User/User.java
+++ src/com/twu/biblioteca/User/User.java
@@ -2,19 +2,19 @@ package com.twu.biblioteca.User;
 
 public class User {
 
-    private static String libraryNumber;
-    private static String password;
+    private String libraryNumber;
+    private String password;
 
     public User(String libraryNumber, String password) {
         this.libraryNumber = libraryNumber;
         this.password = password;
     }
 
-    public static String getLibraryNumber() {
+    public String getLibraryNumber() {
         return libraryNumber;
     }
 
-    public static String getPassword() {
+    public String getPassword() {
         return password;
     }
 }
